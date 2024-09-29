a759df4d8f99d93d001c5b724a98f3bc3993b659
danes <danesnovigar@gmail.com>
2024-09-29 22:13:20 +0700

    master: add new test locator & adjust URL

--- tests/workshop_1/workshop_1.spec.ts
+++ tests/workshop_1/workshop_1.spec.ts
@@ -1,7 +1,7 @@
 import {expect, test} from '@playwright/test';
 
 test('Basic Navigation', async ({page})=>{
-    await page.goto('https://github.com/');
+    await page.goto('https://gitlab.com/');
     await page.waitForTimeout(3000);
     await page.reload()
 })
@@ -16,4 +16,10 @@ test('Interacting with Web Element on Github', async ({page})=>{
     await page.locator('[name="new_user[first_name]"]').fill('testerdonasirofirst');
     await page.locator('#new_user_last_name').fill('testerdonasirolast');
     await page.waitForTimeout(3000);
+})
+
+test('Using Various Locator Methods', async ({page})=>{
+    await page.goto('https://gitlab.com/')
+    await page.getByRole('button', {name: 'Main menu'}).click();
+    await page.getByRole('link', {name: 'Sign in'}).click();
 })
