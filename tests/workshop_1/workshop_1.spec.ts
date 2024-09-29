d6d4dab0f9a10c5a27a0cdafc7b6c1f482c02974
danes <danesnovigar@gmail.com>
2024-09-29 20:43:41 +0700

    main: workshop 1

---
+++ tests/workshop_1/workshop_1.spec.ts
@@ -0,0 +1,19 @@
+import {expect, test} from '@playwright/test';
+
+test('Basic Navigation', async ({page})=>{
+    await page.goto('https://github.com/');
+    await page.waitForTimeout(3000);
+    await page.reload()
+})
+
+test('Interacting with Web Element on Github', async ({page})=>{
+    await page.goto('https://gitlab.com/');
+    // await page.locator('role="banner"');
+    // const bannerLocator = page.locator('[role="banner"]');
+    // await expect(bannerLocator).toBeVisible();
+    // await page.waitForTimeout(3000);
+    await page.locator('#be-navigation-desktop').getByRole('link', {name: ' Get free trial '}).click();
+    await page.locator('[name="new_user[first_name]"]').fill('testerdonasirofirst');
+    await page.locator('#new_user_last_name').fill('testerdonasirolast');
+    await page.waitForTimeout(3000);
+})
